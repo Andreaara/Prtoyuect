648f4d115c508745c7aad7c7099675f6ce1dddf0
andrea <20243ds171@utez.edu.mx>
2025-09-11 09:01:52 -0600

    CAMBIOS

---
+++ src/PromedioSueldo.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PromedioSueldo {
+    public static void main(String[] args) throws Exception {
+
+        List<Worker> trabajadores = new ArrayList<>();
+
+        trabajadores.add(0,"Miguel",200);
+        trabajadores.add(1,"Angel", 3000);
+        trabajadores.add(2, "Aron", 2000);
+        trabajadores.add(3,"Hector",200);
+        trabajadores.add(4,"Juan", 3000);
+        trabajadores.add(5, "Mariana", 2000);
+
+        for(Worker trabajador : trabajadores){
+            System.out.println(trabajador);
+        }
+
+        for(worker trabajador : trabajadore){
+            if(TR)
+        }
+
+
+
+      
+    }
+}
+  
---
+++ src/Worker.java
@@ -0,0 +1,13 @@
+
+public class Worker {
+    private int id;
+    private String nombre;
+    private double sueldo;
+
+    public Worker( int id, String nombre, double sueldo){
+        this.id = id;
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+    }
+
+}
