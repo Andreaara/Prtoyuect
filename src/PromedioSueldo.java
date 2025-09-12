b1cc316cdf573d37ee43af26cdbe26e8f1aa1443
Andrea <correo@correo>
2025-09-11 19:38:07 -0600

    realice trabajo

--- src/PromedioSueldo.java
+++ src/PromedioSueldo.java
@@ -3,27 +3,41 @@ import java.util.List;
 
 public class PromedioSueldo {
     public static void main(String[] args) throws Exception {
+        double sueldo = 0;
+        double cantidad = 0;
 
         List<Worker> trabajadores = new ArrayList<>();
 
-        trabajadores.add(0,"Miguel",200);
-        trabajadores.add(1,"Angel", 3000);
-        trabajadores.add(2, "Aron", 2000);
-        trabajadores.add(3,"Hector",200);
-        trabajadores.add(4,"Juan", 3000);
-        trabajadores.add(5, "Mariana", 2000);
+        trabajadores.add(new Worker(1,"Cesar",2000));
+        trabajadores.add(new Worker(2,"Daniel",200));
+        trabajadores.add(new Worker(3,"Sebastian",50));
+        trabajadores.add(new Worker(4,"Emiliano",60));
+        trabajadores.add(new Worker(5,"Edgar",2000));
+        trabajadores.add(new Worker(6,"Jose",70));
+
 
         for(Worker trabajador : trabajadores){
             System.out.println(trabajador);
         }
 
-        for(worker trabajador : trabajadore){
-            if(TR)
+        for(Worker trabajador : trabajadores){
+            sueldo += trabajador.getSueldo();
+            cantidad++;
+        }
+
+        System.out.println("El Sueldo promedio es de: " + sueldo / cantidad);
+
+        for(Worker trabajador : trabajadores){
+            if(trabajador.getSueldo() <= 1000){
+                trabajadores.remove(trabajador);
+            }else {
+                System.out.println("Lista actualizada: " + trabajadores);
+            }
         }
 
 
 
-      
+
     }
 }
   
--- src/Worker.java
+++ src/Worker.java
@@ -10,4 +10,36 @@ public class Worker {
         this.sueldo = sueldo;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    @Override
+    public String toString() {
+        return "Worker{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", sueldo=" + sueldo +
+                '}';
+    }
 }
