ec683c376f1926619b377102207863d44f0734ac
Andrea Aragón <20243ds171@utez.edu.mx>
2025-10-20 08:48:28 -0600

    Eliminación de duplicados Doublelinkedlist

---
+++ f4/App.java
@@ -0,0 +1,32 @@
+public class App {
+    public static void main(String[] args)  {
+        
+    
+    DoublelinkedList list = new DoublelinkedList();
+    list.printForward();
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(4);
+    list.add(5);
+    list.add(5);
+    list.add(5);
+    list.add(6);
+    list.duplicados();
+    list.printForward();
+    System.out.println("-----ImprimIR Backward------");
+    list.duplicados();
+    list.printBackward();
+
+    System.out.println("------Remove--------");
+    list.remove(4);
+    list.printForward();
+
+    System.out.println("-----------");
+    list.duplicados();
+
+
+    }
+
+    
+}
