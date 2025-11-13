ea37291b36d6e6702e7d78e634ee76b3bdd0b648
Andrea Aragón <20243ds171@utez.edu.mx>
2025-11-13 07:52:41 -0600

    SelectionSort

---
+++ selectionSort/App.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class App{
+
+    public static void main(String[] args) {
+        SelectionSort sorting = new SelectionSort();
+
+        int [] arr={3,7,0,10,4};
+        System.out.println("Arreglo desrodenado");
+        System.out.println(Arrays.toString(arr));
+        System.out.println("Arreglo ordenado");
+        sorting.sort(arr);
+    }
+} 
