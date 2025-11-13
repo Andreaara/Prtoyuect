03030bf56ab11f65c3cf257da230ae819393cf5d
Andrea Aragón <20243ds171@utez.edu.mx>
2025-11-13 08:14:02 -0600

    InsertionSort

---
+++ InsertionSort/App.java
@@ -0,0 +1,11 @@
+public class App{
+
+    public static void main(String[] args)  {
+
+        InsertionSort sorting = new InsertionSort();
+
+        int[] arr= {2,6,7,10,9};
+        sorting.sort(arr);
+        
+    }
+}
